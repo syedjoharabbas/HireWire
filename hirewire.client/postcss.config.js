91e421e3202d6075bb0b435b229840b7a7c984e0
joharabbasshah <joharabbasshah@gmail.com>
2025-09-21 14:28:32 +0400

    Dockker Support

--- hirewire.client/postcss.config.js
+++ hirewire.client/postcss.config.js
@@ -1,9 +1,1 @@
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-
-export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer
-  ]
-}
+// removed; using postcss.config.cjs (CommonJS) for PostCSS configuration in build environments
